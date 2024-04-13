dfac6601c0508e6b550946e7fb77ea6a1c351146
Boaz Bitton <boaz2119@gmail.com>
2024-04-13 18:22:28 +0300

    added styled toogle theme

--- src/components/ToggleTheme/ToggleTheme.tsx
+++ src/components/ToggleTheme/ToggleTheme.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import classes from "./ToggleTheme.module.scss";
 import { ContextOptions, useOptions } from "../../Context";
+import { ReactComponent as Moon } from "../utilis/svg/moon.svg";
+import { ReactComponent as Sun } from "../utilis/svg/sun.svg";
 
 const ToggleTheme = () => {
   const { options, setOptions } = useOptions();
@@ -11,9 +13,18 @@ const ToggleTheme = () => {
     setOptions(newTheme);
   };
   return (
-    <div>
-      {" "}
-      <button onClick={changeTheme}>Click ME</button>
+    <div className={classes.__toggleTheme}>
+      <input
+        onClick={changeTheme}
+        type="checkbox"
+        id="toggleCheckbox"
+        className={classes.__toggle}
+      />
+      <label htmlFor="toggleCheckbox">
+        <Sun className={classes.__sun} />
+
+        <Moon className={classes.__moon} />
+      </label>
     </div>
   );
 };
